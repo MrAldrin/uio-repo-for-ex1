99da911d37e3c764f1a88a2c13d97eefe05e30cf
hsa <hemingsa@math.uio.no>
2020-08-30 12:54:38 +0200

    finished ex3

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,8 @@ def test_add_float_exercise_2():
     a = .1
     b = .2
     assert abs(calculator.add(a,b) - .3) < 1e-6, 'should be < 1e-6'
+
+def test_add_string_exercise_3():
+    a = 'Hello '
+    b = 'World'
+    assert calculator.add(a,b) == 'Hello World', 'should be "Hello World"'
