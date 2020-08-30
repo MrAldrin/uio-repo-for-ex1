f4f55b547bf6aabbb600e8926dba1f2bf2593bd8
hsa <hemingsa@math.uio.no>
2020-08-30 12:43:46 +0200

    finished ex2

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,11 @@
 import calculator
 
-def test_add_exercise_1():
+def test_add_integer_exercise_1():
     a = 1
     b = 2
     assert calculator.add(a,b) == 3, 'should be 3'
+
+def test_add_float_exercise_2():
+    a = .1
+    b = .2
+    assert abs(calculator.add(a,b) - .3) < 1e-6, 'should be < 1e-6'
